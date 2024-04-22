0c45e389fc2963bd4ee132d8321e48c36f354af1
Lobo <elianrodriguesribeiro@gmail.com>
2024-04-22 12:47:52 -0300

    Starting the project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mentalhealthcareawareness',
+  webDir: 'www'
+};
+
+export default config;
